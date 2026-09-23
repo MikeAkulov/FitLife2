8e185b1066fdac2f246a658b65d5e8660ad0c17d
Mike Akulov <akulov-misha@list.ru>
2026-09-23 12:52:28 +0300

    Дописал 2 проект

--- fit_life2.py
+++ fit_life2.py
@@ -1,11 +1,12 @@
 # спрашиваем имя
 
-WATER_PER_KG = 30
+WATER_PER_KG_W = 30
+WATER_PER_KG_M = 35
 STEPS_PER_YEAR = 100
 
 # Цикл имя
 while True:
-    user_name = input("Введите ваше имя: ").strip()
+    user_name = input("Здравствуйте, введите ваше имя: ").strip()
     if len(user_name) > 0:
         break
     else:
@@ -13,7 +14,7 @@ while True:
 # Цикл спрашиваем возраст
 while True:
     try:
-        user_age = int(input("Какой у вас возраст? "))
+        user_age = int(input("Укажите ваш возраст: "))
         if user_age > 0:
             break
         else:
@@ -23,7 +24,7 @@ while True:
 # Цикл спрашиваем вес в кг
 while True:
     try:
-        mass = float(input("Какой у вас вес (например 83.5): "))
+        mass = float(input("Укажите ваш вес(например 83.5): "))
         if mass > 0:
             break
         else:
@@ -33,45 +34,45 @@ while True:
 # спрашиваем рост
 while True:
     try:
-        user_high = float(input("Введите ваш рост через точку: "))
-        if user_high > 0:
-            break
+        user_high = float(input("Укажите ваш рост в метрах(например 1.81): "))
+        if 0 < user_high <= 0.5:
+            print('Рост не может быть меньше 50см')
+        elif user_high >= 2.5:
+            print('Не бывает таких высоких людей!')
+        elif user_high < 0:
+            print('Рост не может быть отрицательным')
         else:
-            print("Рост не может быть отрицательным")
+            break
     except ValueError:
         print("Укажите рост только цифрами!")
+# Спрашиваем пол
+user_sex = int(input('Укажите ваш пол (1 - мужчина, 2 - женщина): '))
+if user_sex == 1:
+    # Считаем норму воды
+    water = float(mass * WATER_PER_KG_M / 1000)
+else:
+    water = float(mass * WATER_PER_KG_W / 1000)
+# Выводим ИМТ
+imt = mass / user_high ** 2
+imt = round(imt, 1)
+if imt <= 18.5:
+    a = "Недостаточный вес"
+elif 25 < imt < 29.9:
+    a = 'Избыточный вес'
+elif 18.5 < imt < 24.9:
+    a = 'Норма'
+else:
+    a = 'Ожирение'
 
 
-# Выводим функцию для рассчета ИМТ
-def imt_user():
-    imt = mass / user_high ** 2
-    if imt < 18.5:
-        print("Недостаточный вес")
-    elif 25 < imt < 29.9:
-        print('Избыточный вес')
-    elif 18.5 < imt < 24.9:
-        print('Норма')
-    else:
-        imt > 30
-        print('Ожирение')
-
-
-# Норма воды
-water = float(mass * WATER_PER_KG / 1000)
 # Норма шагов
 steps_user = user_age * 100
 # Норма сна
-user_sleep = 8 * (user_age // 30) * 0.5
-
-# Отчет для пользователя: Иван, (25 г.)
-# ИМТ: 23.4 — Норма
-# Рекомендуемая норма воды: 2.55 л. в день
-# Рекомендуемая норма шагов: 2500 шагов
-# Рекомендуемая норма сна: 8.0 ч.
-
+user_sleep = 8 - (user_age/30)*0.5
 # Выводим красивый текст
-print(f"Привет {user_name} !")
-print(f"Тебе {user_age} лет")
-print(f"Ваш вес: {mass}")
-print(f"Ваш рост: {user_high}")
-imt_user()
+print(f"Отчет для пользователя: {user_name}, ({user_age}г.)")
+print(f"ИМТ: {imt} — {a}")
+print(f"Рекомендуемая норма воды: {water} л. в день")
+print(f"Рекомендуемая норма шагов: {steps_user} в день")
+print(f"Рекомендуемая норма сна: {user_sleep} ч.")
+print('Расчет окончен. Будьте здоровым!')
